a4f9327b68ae514f08fb9038eb6622687f94445d
Yuqi Wang <8285693+yuqiw18@users.noreply.github.com>
2018-11-23 09:49:25 +0000

    Update computeIntegralImage.m

--- Non-Local Means Denoising/computeIntegralImage.m
+++ Non-Local Means Denoising/computeIntegralImage.m
@@ -1,34 +1,4 @@
 function [integralImage] = computeIntegralImage(image)
-%% Hard Coded
-% This part was done before I fuuly understood the concept
-% It works but is not efficient
-% % Shift the top and left by 1
-% [imageRow,imageCol,~] = size(image);   
-% shiftTop = zeros(1, imageCol+1);
-% shiftLeft = zeros(imageRow,1);
-% image = cat(2, shiftLeft, image);
-% image = cat(1, shiftTop, image);
-% [imageRow,imageCol,~] = size(image);   
-% 
-% % Preallocate
-% integralImage = zeros(imageRow,imageCol); 
-% 
-% % Calculate the integral image
-% for r = 1 : imageRow;
-%     for c = 1 : imageCol
-%         if (r==1 && c ==1) % Handle starting point
-%             integralImage(r,c) = image(r,c);
-%         elseif (r ==1) % Handle left column
-%             integralImage(r,c) = integralImage(r,c-1) + image(r,c);
-%         elseif (c ==1) % Handle top row
-%             integralImage(r,c) = integralImage(r-1,c) + image(r,c);
-%         else % Handle any other condition
-%             integralImage(r,c) = integralImage(r,c-1) + sum(image(1:r-1,c),'all') + image(r,c);
-%         end
-%     end
-% end
-
-%% More Efficient Way
 % As described in lecture slide p56 - use cumulative sum
 % Shift the top and left by 1
 [imageRow,imageCol,~] = size(image);   
