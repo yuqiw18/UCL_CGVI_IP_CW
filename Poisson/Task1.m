79789adb00de20aac69055996e9aa572e2e3bf15
Yuqi <8285693+yuqiw18@users.noreply.github.com>
2018-12-20 18:51:21 +0000

    Update Task1.m

--- Poisson/Task1.m
+++ Poisson/Task1.m
@@ -1,15 +1,39 @@
-% Task 1: Grayscale Image
-image = rgb2gray(imread("./images/portrait.jpg"));
+%% Task 1: Grayscale Image
+clear;
+clc;
 
-% Return binary mask
-maskRegion = roipoly(im2double(image));
+% Load the image
+sourceImage = rgb2gray(imread("./images/portrait.jpg"));
+targetImage = zeros(size(sourceImage));
 
-% Get the boundary
+% Return the binary mask
+maskRegion = roipoly(im2double(sourceImage));
+targetMaskRegion = zeros(size(maskRegion));
+regionPixelCount = find(maskRegion);
+
+% Get the boundary coordinate
 boundary = bwboundaries(maskRegion);
-
-
-%
-% maskArea = maskRegion .* double(image);
+boundaryCoord = cell2mat(boundary);
 
 %
+% 
+% for n =1:size(smallMask_row);
+%     result(smallMask_row(n),smallMask_col(n)) = f(n);
+% end
+% 
+% 
+% 
+% figure;
+% imshow(targetImage);
+
+
+%% Test
+%
+% figure;
+% imshow(maskRegion);
+% title("Mask Region");
+%
+% figure;
+% maskArea = maskRegion .* double(sourceImage);
 % imshow(uint8(maskArea));
+% title("Selected Region");
