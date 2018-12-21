0ae80532ea385edb60d61f303cc27627b8bf15a0
Yuqi <8285693+yuqiw18@users.noreply.github.com>
2018-12-21 21:34:34 +0000

    Update Task1.m

--- Poisson/Task1.m
+++ Poisson/Task1.m
@@ -8,24 +8,62 @@ targetImage = zeros(size(sourceImage));
 
 % Return the binary mask
 maskRegion = roipoly(im2double(sourceImage));
-targetMaskRegion = zeros(size(maskRegion));
+%targetMaskRegion = zeros(size(maskRegion));
 regionPixelCount = find(maskRegion);
+maskRegionValue = maskRegion .* double(sourceImage);
 
 % Get the boundary coordinate
 boundary = bwboundaries(maskRegion);
 boundaryCoord = cell2mat(boundary);
 
-%
-% 
-% for n =1:size(smallMask_row);
-%     result(smallMask_row(n),smallMask_col(n)) = f(n);
-% end
-% 
-% 
-% 
-% figure;
-% imshow(targetImage);
+% Mask region excluding the boundary
+exBoundaryMaskRegion = maskRegion;
+for i = 1:size(boundaryCoord,1)
+    % Crop the boundary from original mask
+    exBoundaryMaskRegion(boundaryCoord(i,1),boundaryCoord(i,2))=0;
+end
+exBoundaryMaskRegionValue = exBoundaryMaskRegion .* double(sourceImage);
+exBoundaryMaskRegionPixelCount = find(exBoundaryMaskRegionValue);
 
+exBoundaryMaskOrder = zeros(size(exBoundaryMaskRegionValue));
+for i = 1:size(exBoundaryMaskRegionPixelCount)
+    exBoundaryMaskOrder(exBoundaryMaskRegionPixelCount(i))=i;
+end
+
+A = delsq(exBoundaryMaskOrder);
+B = delsq(exBoundaryMaskRegionValue);
+
+% Blur the bounday
+targetImageMaskRegionBoundaryCoord = boundaryCoord;
+targetUnageMaskRegionBoundaryValue = zeros(size(maskRegionValue));
+for i = 1 : size(boundaryCoord,1)
+    targetUnageMaskRegionBoundaryValue(boundaryCoord(i,1),boundaryCoord(i,2))=sourceImage(boundaryCoord(i,1),boundaryCoord(i,2));
+end
+
+[maskXCoord, maskYCoord] = find(maskRegion);
+for i =1:size(maskXCoord)
+    neighbour1 = targetUnageMaskRegionBoundaryValue(maskXCoord(i)-1, maskYCoord(i));
+    neighbour2 = targetUnageMaskRegionBoundaryValue(maskXCoord(i)+1, maskYCoord(i));
+    neighbour3 = targetUnageMaskRegionBoundaryValue(maskXCoord(i), maskYCoord(i)-1);
+    neighbour4 = targetUnageMaskRegionBoundaryValue(maskXCoord(i), maskYCoord(i)+1);
+    targetImage(maskXCoord(i), maskYCoord(i)) = neighbour1 + neighbour2 + neighbour3 + neighbour4 - 4* targetUnageMaskRegionBoundaryValue(maskXCoord(i), maskYCoord(i));
+end
+
+% for i = 1 : size (boundaryCoord, 1)
+%     targetImage(boundaryCoord(i,1),boundaryCoord(i,2))=2;
+% end
+
+fq = targetImage(exBoundaryMaskRegionPixelCount);
+f = A\fq;
+[xX, yY] = find(exBoundaryMaskRegion);
+result = double(sourceImage);
+
+for i = 1:size(xX)
+    result(xX(i),yY(i))=f(i);
+end
+
+figure;
+imshow(result/255);
 
 %% Test
 %
@@ -34,6 +72,11 @@ boundaryCoord = cell2mat(boundary);
 % title("Mask Region");
 %
 % figure;
-% maskArea = maskRegion .* double(sourceImage);
-% imshow(uint8(maskArea));
+% maskRegionValue = maskRegion .* double(sourceImage);
+% imshow(uint8(maskRegionValue));
 % title("Selected Region");
+% 
+% figure;
+% maskArea2 = exBoundaryMaskRegion .* double(sourceImage);
+% imshow(uint8(maskArea2));
+% title("Selected Region without boundary");
